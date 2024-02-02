40ea04445307241aa1be0b9c77f339b574dad50b
xiaocanmeng <2585609923@qq.com>
2024-02-02 16:37:54 +0900

    http

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 // main.cpp
 #include "DiaryApp.h"
-
+#include "server.h"
 int main() {
-    DiaryApp diaryApp("diary.db");
-    diaryApp.startup();
-
+    // DiaryApp diaryApp("diary.db");
+    // diaryApp.startup();
+    Servertest();
     return 0;
 }
---
+++ src/server.cpp
@@ -0,0 +1,27 @@
+#include "httplib.h"
+#include "server.h"
+
+int32_t Servertest()
+{
+    httplib::Server server;
+
+    // 处理 GET 请求
+    server.Get("/hello", [](const httplib::Request &req, httplib::Response &res)
+               { res.set_content("Hello, World!", "text/plain"); });
+
+    // 处理 POST 请求
+    server.Post("/echo", [](const httplib::Request &req, httplib::Response &res)
+                { res.set_content(req.body, "text/plain"); });
+
+    // 启动服务器，监听端口8080
+    if (server.listen("172.23.32.17", 8081))
+    {
+        std::cout << "Server started at http://172.23.32.17:8081" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Error starting server" << std::endl;
+    }
+
+    return 0;
+}
