e0f7f0aec95307686dbb3a414dbff46ff2de7db2
xiaocanmeng <2585609923@qq.com>
2024-03-05 15:34:04 +0900

    add

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def hello():
