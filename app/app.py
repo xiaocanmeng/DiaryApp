066a0e046fa8e5a803b2eacd1cc290f89542323b
xiaocanmeng <2585609923@qq.com>
2024-03-05 15:12:12 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is a simple HTTP server.'
+    template_path = os.path.join('index.html')
+    return render_template(template_path, message='Hello, World! This is a simple HTTP server.')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
