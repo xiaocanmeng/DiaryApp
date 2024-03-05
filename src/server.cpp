bee14dc7509a3c869c9b6237702a8bb6d27dad21
xiaocanmeng <2585609923@qq.com>
2024-03-05 15:01:17 +0900

    http server

--- src/server.cpp
+++ src/server.cpp
@@ -14,9 +14,9 @@ int32_t Servertest()
                 { res.set_content(req.body, "text/plain"); });
 
     // 启动服务器，监听端口8080
-    if (server.listen("172.23.32.17", 8081))
+    if (server.listen("172.20.143.35", 8081))
     {
-        std::cout << "Server started at http://172.23.32.17:8081" << std::endl;
+        std::cout << "Server started at http://172.20.143.35:8081" << std::endl;
     }
     else
     {
